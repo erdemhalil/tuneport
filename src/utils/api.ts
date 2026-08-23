6e60a6dd3e00d732ee99a5cf068f39b9ca56f365
erdemhalil <erdem-2001@windowslive.com>
2026-08-23 14:39:25 +0300

    Use type-only import to fix Turbopack build

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,7 +2,7 @@ import { httpBatchLink, loggerLink } from "@trpc/client";
 import { createTRPCNext } from "@trpc/next";
 import superjson from "superjson";
 
-import { type AppRouter } from "~/server/api/root";
+import type { AppRouter } from "~/server/api/root";
 
 const getBaseUrl = () => {
   if (typeof window !== "undefined") return ""; // browser should use relative url
